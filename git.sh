e6288c440dced9db0c7c0e3dc4a0a037dd50fc12
qinglong <qinglong@@users.noreply.github.com>
2021-08-25 19:42:28 +0800

    2021-08-25

--- git.sh
+++ git.sh
@@ -1,7 +1,4 @@
-cp /ql/log/x-dr_bd_main_jd_bean_change_new/$(date +%Y-%m-%d)*.log /ql/gitlog
-rm jd.log
-mv $(date +%Y-%m-%d-%H)*.log jd.log
+cp /ql/log/x-dr_bd_main_jd_bean_change_new/$(date +%Y-%m-%d)*.log /ql/gitlog/jd.log
 git add .
 git commit -m "$(date +%Y-%m-%d)"
 git push -f
-
